b7ae508a6d2221bfc65aa6c38552d2585fce494d
Nithish Kumar <nithishlearning45@gmail.com>
2024-11-08 20:59:51 +0530

    Mulithreading Initial Commit

---
+++ src/test/java/com/java/multithreading/JavaMultithreading/JavaMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.multithreading.JavaMultithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMultithreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
