3ed4ce13d6fc5936e02a4db9da339f2eba2cba5d
Nithish Kumar <nithishlearning45@gmail.com>
2024-11-11 17:50:54 +0530

    ReentrantLock & ThreadSafety

---
+++ src/main/java/com/java/multithreading/JavaMultithreading/concurrency/RentrantLockEx.java
@@ -0,0 +1,31 @@
+package com.java.multithreading.JavaMultithreading.concurrency;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class RentrantLockEx {
+    Lock lock = new ReentrantLock();
+    int count =0;
+    public void increment(){
+        lock.lock();
+        try{
+            count++;
+            System.out.println("Reentrant Lock count: "+count);
+        }finally {
+            lock.unlock();
+        }
+    }
+    public static void main(String[] args) {
+        RentrantLockEx rt  = new RentrantLockEx();
+        Thread t1 = new Thread(rt::increment);
+        Thread t2 = new Thread(rt::increment);
+
+        t1.start();
+        t2.start();
+
+
+
+
+
+    }
+}
