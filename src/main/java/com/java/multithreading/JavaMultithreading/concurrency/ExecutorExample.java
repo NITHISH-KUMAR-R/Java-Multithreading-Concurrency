f49f7c7cf9163c6324fe8a8b78a26dd89c8d23db
Nithish Kumar <nithishlearning45@gmail.com>
2024-11-11 18:01:05 +0530

    ExecutorService example

---
+++ src/main/java/com/java/multithreading/JavaMultithreading/concurrency/ExecutorExample.java
@@ -0,0 +1,17 @@
+package com.java.multithreading.JavaMultithreading.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorExample {
+    public static void main(String[] args) {
+        ExecutorService ex = Executors.newFixedThreadPool(5);
+
+        for(int i =1;i<5;i++){
+            int taskId =i;
+            ex.submit(()->{
+            System.out.println("Thread name: "+Thread.currentThread().getName() +" Task ID "+taskId);});
+        }
+        ex.shutdown();
+    }
+}
