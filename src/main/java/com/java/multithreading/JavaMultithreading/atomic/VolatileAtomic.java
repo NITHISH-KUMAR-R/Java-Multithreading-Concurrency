809a0f56de5b5f8ab48b746a20f49cc53d0e4ec4
Nithish Kumar <nithishlearning45@gmail.com>
2024-11-14 23:37:08 +0530

    Atomic and volatile

---
+++ src/main/java/com/java/multithreading/JavaMultithreading/atomic/VolatileAtomic.java
@@ -0,0 +1,42 @@
+package com.java.multithreading.JavaMultithreading.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class VolatileAtomic {
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    private static  volatile boolean stopFlag = false;
+    private static void doWork(){
+        while(!stopFlag){
+            atomicInteger.incrementAndGet();
+            try {
+                Thread.sleep(100);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(VolatileAtomic::doWork);
+        Thread t2 = new Thread(VolatileAtomic::doWork);
+
+        t1.start();
+        t2.start();
+
+        try {
+            Thread.sleep(1000);
+            stopFlag=true;
+            t1.join();
+            //t2.start();
+           // stopFlag=false;
+            t2.join();
+            //stopFlag=true;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("count: " + atomicInteger.get());
+
+    }
+}
