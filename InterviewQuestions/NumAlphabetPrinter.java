845227042b66a46bb745613a69a266c823632a60
Nithish Kumar <nithishlearning45@gmail.com>
2025-05-20 09:39:27 +0530

    LLD interview

--- InterviewQuestions/NumAlphabetPrinter.java
+++ InterviewQuestions/NumAlphabetPrinter.java
@@ -1,4 +1,55 @@
 package com.JavaMultithreading.InterviewQuestions;
 
 public class NumAlphabetPrinter {
+
+    public String nithishLock = "nithish";
+    public volatile  boolean isNumberTurn = true;
+    Runnable r1 = ()->{
+
+        for(int i =1;i<=26;i++){
+            synchronized(nithishLock){
+                while(!isNumberTurn){
+                    try {
+                        nithishLock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println(" Number Turn: "+i);
+                isNumberTurn = false;
+                nithishLock.notify();
+
+            }
+        }
+
+    };
+
+    Runnable r2 = ()->{
+
+        for(char i ='a';i<='z';i++){
+            synchronized(nithishLock){
+                while(isNumberTurn){
+                    try {
+                        nithishLock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println(" Alpha Turn: "+i);
+                isNumberTurn = true;
+                nithishLock.notify();
+
+            }
+        }
+
+    };
+
+
+
+    public static void main(String a[]){
+
+        NumAlphabetPrinter printer = new NumAlphabetPrinter();
+        new Thread(printer.r1).start();
+        new Thread(printer.r2).start();
+    }
 }
---
+++ InterviewQuestions/OddEven.java
@@ -0,0 +1,57 @@
+package com.JavaMultithreading.InterviewQuestions;
+
+public class OddEven {
+
+    public Object nithish =  new Object();
+
+    public volatile  int i =0;
+
+    Runnable r1 = ()->{
+
+        while(i<40){
+            synchronized (nithish) {
+                if (i % 2 == 0) {
+                    System.out.println("EVen " + i);
+                    i++;
+                    nithish.notify();
+                } else {
+                    try {
+                        nithish.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+
+    };
+
+
+    Runnable r2 = ()->{
+       while(i<40) {
+            synchronized (nithish) {
+                if (i %2!= 0) {
+                    System.out.println("Odd " + i);
+                    i++;
+                    nithish.notify();
+                } else {
+                    try {
+                        nithish.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+
+    };
+    public static void main(String a[]){
+
+        OddEven obj = new OddEven();
+        new Thread(obj.r1).start();
+        new Thread(obj.r2).start();
+
+
+
+    }
+}
